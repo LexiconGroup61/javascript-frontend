71df14f64aaf768011e065eacaaf92345af317f7
tobiasengberg <tobias.engberg@exilic.se>
2026-05-22 09:52:24 +0200

    SVG

--- scripts/buttonClicking.js
+++ scripts/buttonClicking.js
@@ -16,3 +16,25 @@ buttonTarget.addEventListener("click", (event) => {
         toChange.innerText = "No button clicked"
     }
 })
+
+window.addEventListener("load", (e) => {
+    let newSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    newSVG.setAttribute("width", "100");
+    newSVG.setAttribute("height", "100");
+    let newRectA = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    newRectA.setAttribute("x", "0");
+    newRectA.setAttribute("y", "0");
+    newRectA.setAttribute("width", "100");
+    newRectA.setAttribute("height", "100");
+    newRectA.setAttribute("class", "bg-square");
+    newSVG.appendChild(newRectA);
+    let newRect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    newRect.setAttribute("x", "20");
+    newRect.setAttribute("y", "20");
+    newRect.setAttribute("width", "50");
+    newRect.setAttribute("height", "50");
+    newRect.setAttribute("class", "square");
+    newSVG.appendChild(newRect);
+    document.querySelector("#site-container").appendChild(newSVG);
+
+})
